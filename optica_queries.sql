4feae8d424a7188e8681ee1390a829a0f1f7d158
Javier Castillo <javiercame98@gmail.com>
2026-01-22 11:18:21 +0100

    fix: cerating verifications

--- optica_queries.sql
+++ optica_queries.sql
@@ -12,3 +12,23 @@ JOIN customer c ON sa.customer_id = c.customer_id
 JOIN employee e ON sa.employee_id = e.employee_id
 JOIN glasses g ON sa.glasses_id = g.glasses_id
 JOIN brand b ON g.brand_id = b.brand_id;
+
+
+SELECT COUNT(sale_id) AS total_facturas
+FROM sale
+WHERE customer_id = 1 -- Sustituir por el ID del cliente
+  AND sale_date BETWEEN '2024-01-01' AND '2024-12-31';
+
+SELECT DISTINCT b.name AS modelo_marca
+FROM sale s
+         JOIN glasses g ON s.glasses_id = g.glasses_id
+         JOIN brand b ON g.brand_id = b.brand_id
+WHERE s.employee_id = 1 -- Sustituir por el ID del empleado
+  AND YEAR(s.sale_date) = 2024;
+
+SELECT DISTINCT p.name AS proveedor
+FROM supplier p
+         JOIN brand b ON p.supplier_id = b.supplier_id
+         JOIN glasses g ON b.brand_id = g.brand_id
+         JOIN sale s ON g.glasses_id = s.glasses_id;
+
